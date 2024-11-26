208c6a6df4014ed9c48d19614b4d951f5e3368e7
Neal Schwarz <Neal.schwarz001@student.swic.educlear>
2024-11-25 20:42:02 -0600

    add code to main js

--- src/main.js
+++ src/main.js
@@ -1,2 +1,23 @@
+import Button from "./components/button";
+import Count from "./components/count";
+import state from "./state";
 
+const app = document.querySelector("#app");
 
+function render() {
+  app.innerHTML = `  ${Count(state.getState().count)}
+    <div class="flex justify-center">
+      ${Button("+1", "increment")}
+      ${Button("-1", "decrement")}
+    </div>`;
+}
+
+state.subscribe(render);
+render();
+
+document.addEventListener("click", (event) => {
+  if (event.target.id === "increment")
+    state.setState({ count: state.getState().count + 1 });
+  else if (event.target.id === "decrement")
+    state.setState({ count: state.getState().count - 1 });
+});
